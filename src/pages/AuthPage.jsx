e575910dd589a55a060380459d586942539e241d
GIHOZO Ismail <gihozoismail@gmail.com>
2026-07-18 16:17:52 +0200

    allow log with google

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -546,7 +546,7 @@ const AuthPage = () => {
 
   const handleGoogleAuth = () => {
     const base = import.meta.env.VITE_API_BASE_URL || "";
-    window.location.href = `${base}/api/v1/users/google/login/`;
+    window.location.href = `${base}/api/v1/users/google/`;
   };
 
   useEffect(() => {
